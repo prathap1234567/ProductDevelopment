e9fb727edbeab2a430a2739c50f71d5f361c7428
prathap1234567 <110650154+prathap1234567@users.noreply.github.com>
2023-09-25 12:43:35 +0530

    Add files via upload

---
+++ Day9/src/Terms.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Terms.css';
+function Terms() {
+return (
+    <div>
+        <h1>TERMS AND CONDITIONS FOR DISASTER RELIEF CROWDFUNDING APP</h1>
+
+<h2>INTRODUCTION</h2> 
+  
+<p>You are solely responsible for utilizing the funds for the Purpose mentioned in our fund-raising page. The Company is not responsible in any way whatsoever towards the end utilization of funds.
+This fund-raising page on the Platform will be available to the general public for contributions for 25 days from the time You published the page on the Platform. You may or may not be able to raise the targeted amount in the designated period. The Company at its sole discretion may extend the tenor of hosting or may remove this fund-raising page.
+The Company will not be responsible in any way whatsoever on the amount of contributions received.
+The Company will collect contributions online, and will provide the total amount to us. Therefore, you shall receive the funds in 29 days from the time you list the project. The Platform will provide us the list of contributors, and their respective contribution amount at the time of transfer of funds..</p>
+
+
+
+<h2>PROJECT COMPLETION</h2>
+
+<p>While campaigners have full project ownership and the right to complete it, they are accountable and obliged to finish the project by the deadline they have set. Campaigners must also keep the funders updated about the work in progress in case of delays to avoid funder dissatisfaction. The Company or the Site does not make any claims or guarantees on behalf of the campaigner regarding the completion of the project and usage of the funds raised solely for the purpose of the project. Similarly, the Company or the Website does not take any responsibility for making sure that the project for which the funds are raised through its Site is completed and made available to the contributors. The contributors are free to get in touch with the campaign owner directly and solving any dispute that arises regarding the campaign or the project.</p>
+
+
+
+<h2>GENERAL TERMS</h2>
+
+<p>The Purpose, which the Recipient has been clearly declared on the fund-raising page, and I have clearly understood the project and the utilization of funds.
+My contributions will go to the Recipient, and will be utilized by the Recipient. The Company is only facilitating the collection of contributions, and is not responsible in any way for the end utilization of the contributions.
+ The Company and the payment gateway will deduct their fees and applicable taxes before providing the funds to the Recipient.
+ The Company will deduct the fees on behalf of the platform which has hosted the project profile in the internet, and has facilitated my contributions to the project. I understand that the fees are towards web hosting, marketing, and promotion, and that the Company is only facilitating in collection of the fees on behalf of the owners of the Platform.
+Any receipt or Tax Deduction Certificate (for Indian contributors only) will be issued by the Recipient to the extent of the funds received, post deduction by Milaap and the payment gateway. Whether such receipt will be provided has been clearly mentioned in the fun-raising page, and I have understood the same. The Company will not be responsible for any Tax certificates or benefits.
+The Company will collect contributions from many sources and then provide the collective amount to the Recipient; therefore, my contribution may take 45 days – from the time I have made it – to reach the Recipient.
+This Agreement shall be governed by and construed under the applicable laws of the Indian Union, and in respect of any suit, action or proceeding arising out of this Agreement to the jurisdiction of the courts of Bengaluru, India only.
+This Agreement shall be valid for a period of 180 days from the date of making the contribution. The Company can terminate the Agreement by providing a written notice of 30 days.</p>
+
+<ul>
+   
+</ul>
+
+<h2>YOUR CONTENT</h2>
+
+<p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant pharmacy management system a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
+
+<p>Your Content must be your own and must not be invading any third-party's rights. pharmacy management system reserves the right to remove any of Your Content from this Website at any time without notice.</p>
+
+<h2>No warranties</h2>
+
+<p>This Website is provided "as is," with all faults, and crowdfunding management system express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+
+<h2>LIMITATIONS OF LIABILITY</h2>
+
+<p>In no event shall crowfunding management system, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  pharmacy management system, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+
+
+<h2>VARIATION OF TERMS</h2>
+
+<p>Disaster relief management system is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+
+<h2>Assignment</h2>
+
+<p>The  management system is allowed to assign, transfer, and subcontract its rights and / or the obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+<h2>ENTIRE AGREEMENT</h2>
+    
+<p>These Terms constitute the entire agreement between disaster relief crowfunding management system and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
+
+<h2>GOVERING LAW & JURISDICTION</h2>
+
+<p>These Terms will be governed by and interpreted in accordance with the laws of the State of in, and you submit to the non-exclusive jurisdiction of the state and federal courts located in in for the resolution of any disputes.</p>
+    </div>
+  )
+}
+export default Terms;
